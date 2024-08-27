5fe510ea608897e88af75973b9a98140c6c024dc
alejodotcom <alejomelendez82@gmail.com>
2024-08-27 12:51:34 -0600

    switch mode, question sections and end quiz

--- src/main.js
+++ src/main.js
@@ -1,19 +1,7 @@
-import './style.css';
-import viteLogo from '/vite.svg';
-import { setupCounter } from '@/components/counter.js';
+/* Toggle */
+const darkModeToggle = document.getElementById('darkModeToggle');
+const body = document.body;
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-setupCounter(document.querySelector('#counter'));
+darkModeToggle.addEventListener('change', () => {
+  body.classList.toggle('dark-mode');
+});
